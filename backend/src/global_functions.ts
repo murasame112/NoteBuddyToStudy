7e54c7212617070703f1e4397d0d0581c808f57b
murasame112 <tomaszwiesek00@gmail.com>
2023-06-14 16:02:51 +0200

    insert item

--- backend/src/global_functions.ts
+++ backend/src/global_functions.ts
@@ -1,20 +1,44 @@
 import { MongoClient } from 'mongodb';
 import {uri, db_name} from './mongodb/connection';
 
+// https://www.mongodb.com/docs/drivers/node/current/usage-examples/
 
+// function gets item from database by item's id
+// params: id of an item, name of the table (collection) we are searching in
+// returns promise of object from database
 export async function getItemById(id: number, table_name: string) {
     const client = new MongoClient(uri);
     const database = client.db(db_name);
   try {
-    const items: any = database.collection(table_name);
+    const table: any = database.collection(table_name);
     
-    const item = await items.findOne({_id: id});
+    const item = await table.findOne({_id: id});
     return item;
   } finally {
     await client.close();
   }
-
 }
+
+// function inserts item to database
+// params: item, name of the table
+// returns promise of result object (acknowledged: true/false and insertedId: id of inserted item)
+export async function insertItem(item: any, table_name: string) {
+    const client = new MongoClient(uri);
+    const database = client.db(db_name);
+  try {
+    const table: any = database.collection(table_name);
+    
+    const res = await table.insertOne(item);
+    return res;
+  } finally {
+    await client.close();
+  }
+}
+
+
+
+// delete, delete multiple, get multiple items by field, update, update multiple, replace
+
 
 
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,30 @@
 import express from 'express';
 import { Console } from 'console';
 import { Request, Response } from 'express';
- import { getItemById } from './global_functions';
+ import { getItemById, insertItem } from './global_functions';
 
 const app = express() 
 app.use(express.json())
 app.get('/', function (req, res) { 
     
     
-    const note = getItemById(2, 'notes');
-    note.then((value) => {
-        console.log(value);
-        res.send(value);
-      });
+    // const note = getItemById(2, 'notes');
+    // note.then((value) => {
+    //     console.log(value);
+    //     res.send(value);
+    //   });
     
+    
+    //   let x = {
+    //   "_category_id":2,
+    //   "_name":"Pozytywizm"
+    // }
+      // let y = insertItem(x, 'subcategories');
+    // y.then((value) => {
+    //   console.log(value);
+    // });
+    
+    res.send('hello world');
     
      
 }) ;
