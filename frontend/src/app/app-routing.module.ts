d5fdceb6bf0059fbc2fd22c7ba9a29c08204516b
Storm-77 <czechu600@gmail.com>
2023-07-05 13:27:58 +0200

    branch

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { PageErrorComponent } from './components/page-error/page-error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:"landing", pathMatch:"full"},
+  {path:'landing', component:LandingPageComponent},
+  {path:'**', component:PageErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { PageErrorComponent } from './components/page-error/page-error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    PageErrorComponent
   ],
   imports: [
     BrowserModule,
