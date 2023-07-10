1ff79e14a229b15b43d65bedb5d3145a7c57351f
mstasiowski <michal.stasiowski@microsoft.wsei.edu.pl>
2023-07-10 13:29:29 +0200

    zarys

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { PageErrorComponent } from './components/page-error/page-error.component';
 import { FeaturesComponent } from './components/landing-page/features/features.component';
+import { CoursesComponent } from './components/landing-page/courses/courses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     PageErrorComponent,
-    FeaturesComponent
+    FeaturesComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
