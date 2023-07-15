127c75010e02eac4ca1f0a73024727edb4656f1d
murasame112 <tomaszwiesek00@gmail.com>
2023-07-15 16:17:08 +0200

    do testów

--- backend/src/endpoints/category_endpoints.ts
+++ backend/src/endpoints/category_endpoints.ts
@@ -12,7 +12,19 @@ const table_name = 'categories';
 // example:
 //  http://localhost:3000/category/648c6400e388683aeb23d331
 export function getCategoryById(req: Request, res: Response) {
+    const id = req.params.id;
+    const result = global.getItemById(id, table_name);
+    let cat: Category; 
+    result.then((value) => {
+        cat = new Category(
+            value.name
+        );
+        res.send(cat);   
+    });
 }
+
+// TODO: getAllCategories
+
 
 // finds all categories
 // /categories
@@ -29,6 +41,23 @@ export function getAllCategories(req: Request, res: Response) {
 // example:
 //  http://localhost:3000/categories/name&custom_name
 export function getCategoriesByQuery(req: Request, res: Response) {
+    const field = req.params.field;
+    const value = req.params.value;
+    let query = {[field]: JSON.parse(value)};
+    const result = global.getItemsByField(query, table_name);
+    const catArray: Category[] = []; 
+    let cat: Category;
+    result.then((value) => {
+        value.forEach((element: Category) => {
+            
+            cat = new Category(
+                element.name
+            );
+            catArray.push(cat);
+
+        });
+        res.send(catArray);   
+    });
 }
 
 // inserts category to database
@@ -42,6 +71,13 @@ export function getCategoriesByQuery(req: Request, res: Response) {
 //      "name":"custom name",
 // }
 export function insertCategory(req: Request, res: Response) {
+    const cat: Category = new Category(
+        req.body.name
+    );
+    const result = global.insertItem(cat, table_name);
+    result.then((value) => {
+        (value.acknowledged ? res.status(201).send('id: ' + value.insertedId) : res.status(400).send('Error'));
+    });
 }
 
 
@@ -61,6 +97,24 @@ export function insertCategory(req: Request, res: Response) {
     // }
 //  ]
 export function insertMultipleCategories(req: Request, res: Response) {
+    const categories = req.body;
+    let counter = 0;
+    categories.forEach((element: Category) => {
+        const cat: Category = new Category(
+            element.name
+        );
+            
+        const result = global.insertItem(cat, table_name);
+        result.then((value) => {
+            counter ++;
+            if(value.acknowledged == false){
+                res.status(400).send('Error');
+            }
+            if(counter == categories.length){
+                res.status(204).send();
+            }
+        });
+    });
 }
 
 // deletes category by id
@@ -68,6 +122,11 @@ export function insertMultipleCategories(req: Request, res: Response) {
 // example:
 //  http://localhost:3000/category/6490d3e5982efd2fe9136154
 export function deleteCategory(req: Request, res: Response) {
+    const id = req.params.id;
+    const result = global.deleteItemById(id, table_name);
+    result.then((value) => {
+        (value.acknowledged ? res.status(204).send() : res.status(400).send('Error'));
+    });
 }
 
 // deletes multiple categories by array of ids
@@ -82,6 +141,21 @@ export function deleteCategory(req: Request, res: Response) {
 //  "6490d9fddfd298aad1e8f136"]
 
 export function deleteMultipleCategories(req: Request, res: Response) {
+    const ids = req.body;
+    let counter = 0;
+    ids.forEach((element: string) => {
+            
+        const result = global.deleteItemById(element, table_name);
+        result.then((value) => {
+            counter ++;
+            if(value.acknowledged == false){
+                res.status(400).send('Error');
+            }
+            if(counter == ids.length){
+                res.status(204).send();
+            }
+        });
+    });
 }
 
 
@@ -90,6 +164,13 @@ export function deleteMultipleCategories(req: Request, res: Response) {
 // example:
 //  http://localhost:3000/categories/name&custom_name
 export function deleteCategoriesByQuery(req: Request, res: Response) {
+    const field = req.params.field;
+    const value = req.params.value;
+    let query = {[field]: JSON.parse(value)};
+    const result = global.deleteItemsByField(query, table_name);
+    result.then((value) => {
+        (value.acknowledged ? res.status(201).send() : res.status(400).send('Error'));
+    }); 
 }
 
 // updates category by id with values passed in request body
@@ -103,6 +184,12 @@ export function deleteCategoriesByQuery(req: Request, res: Response) {
 //      "name":"custom name"
 // }
 export function updateCategory(req: Request, res: Response) {
+    const id = req.params.id;
+    const query = req.body;
+    const result = global.updateItemById(id, table_name, query);
+    result.then((value) => {
+        (value.acknowledged ? res.status(204).send() : res.status(400).send('Error'));
+    });
 }
 
 // updates multiple categories by array of ids
@@ -123,6 +210,22 @@ export function updateCategory(req: Request, res: Response) {
 //     }
 //  }
 export function updateMultipleCategories(req: Request, res: Response) {
+    const ids = req.body.ids;
+    const updateQuery = req.body.query;
+    let counter = 0;
+    ids.forEach((element: string) => {
+            
+        const result = global.updateItemById(element, table_name, updateQuery);
+        result.then((value) => {
+            counter ++;
+            if(value.acknowledged == false){
+                res.status(400).send('Error');
+            }
+            if(counter == ids.length){
+                res.status(204).send();
+            }
+        });
+    });
 }
 
 // updates multiple categories by field and value
@@ -136,6 +239,14 @@ export function updateMultipleCategories(req: Request, res: Response) {
 //      "name":"custom name"
 // }
 export function updateCategoriesByQuery(req: Request, res: Response) {
+    const field = req.params.field;
+    const value = req.params.value;
+    const updateQuery = req.body;
+    let query = {[field]: JSON.parse(value)};
+    const result = global.updateItemsByField(query, table_name, updateQuery);
+    result.then((value) => {
+        (value.acknowledged ? res.status(204).send() : res.status(400).send('Error'));
+    }); 
 }
 
 
@@ -148,5 +259,11 @@ export function replaceCategory(req: Request, res: Response) {
 // /stealcategory/{id}
 // example:
 //  http://localhost:3000/stealcategory/6490d3e5982efd2fe9136154
+// TODO: poprawić, żeby zwracało obiekt typu Category
 export function stealCategory(req: Request, res: Response) {
+    const id = req.params.id;
+    const result = global.stealItemById(id, table_name);
+    result.then((value) => {
+        res.status(201).send(value.value);
+    });
 }
