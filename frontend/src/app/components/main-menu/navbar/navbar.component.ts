7efa2b2588f0c1963895b98a42c29fcfe5cd7561
mstasiowski <michal.stasiowski@microsoft.wsei.edu.pl>
2023-07-27 13:49:45 +0200

    navbar-menu

--- frontend/src/app/components/main-menu/navbar/navbar.component.ts
+++ frontend/src/app/components/main-menu/navbar/navbar.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+
+  navbarState:boolean = false;
+
+  ngOnInit(): void {
+    const navContainer:any = document.querySelector('#containerNav');
+    let navBtn:any = document.querySelectorAll('.nav-btn');
+    const observer:any = new ResizeObserver((entires)=>{
+      if(entires[0].borderBoxSize[0].inlineSize>650)
+      {
+        navBtn.forEach((e:any)=> {
+          e.style.display="block";
+        });
+        console.log('done');
+      }else
+      {
+        if(this.navbarState ==false)
+        {
+          navBtn.forEach((e:any)=> {
+            e.style.display="none";
+          });
+        }
+
+      }
+    });
+
+    observer.observe(navContainer);
+  }
+
+
+
+  menuBarOpen()
+  {
+    this.navbarState = true;
+    let hamburgerBar:any = document.querySelector('#hamburgerBar');
+    let hamburgerIcon:any = document.querySelector('#hamburgerIcon');
+    let hamburgerIconClose:any = document.querySelector('#hamburgerIcon-close');
+    let navBtn:any = document.querySelectorAll('.nav-btn');
+    let menuBtn:any = document.querySelectorAll('.menu-btn');
+
+    hamburgerBar.style.display = "block";
+    hamburgerIcon.style.display = "none";
+    hamburgerIconClose.style.display = "block";
+
+
+    menuBtn.forEach((e:any)=> {
+      e.style.display="block";
+      e.style.color = "red";
+    });
+
+
+    if(window.innerWidth<=650)
+    {
+
+    navBtn.forEach((e:any)=> {
+      e.style.display="block";
+    });
+
+  }
+
+
+  }
+
+  menuBarClose()
+  {
+    this.navbarState= false;
+    let hamburgerBar:any = document.querySelector('#hamburgerBar');
+    let hamburgerIcon:any = document.querySelector('#hamburgerIcon');
+    let hamburgerIconClose:any = document.querySelector('#hamburgerIcon-close');
+    let navBtn:any = document.querySelectorAll('.nav-btn');
+    let menuBtn:any = document.querySelectorAll('.menu-btn');
+
+    hamburgerBar.style.display = "none";
+    hamburgerIcon.style.display = "block";
+    hamburgerIconClose.style.display = "none";
+
+    menuBtn.forEach((e:any)=> {
+      e.style.display="none";
+    });
+
+    if(window.innerWidth<=650)
+    {
+
+    navBtn.forEach((e:any)=> {
+      e.style.display="none";
+    });
+
+  }
+}
 
 }
