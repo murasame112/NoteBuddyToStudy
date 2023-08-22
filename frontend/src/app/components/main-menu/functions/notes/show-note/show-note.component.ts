347d667de536e1aa9089519869702b76e86da4d2
mstasiowski <michal.stasiowski@microsoft.wsei.edu.pl>
2023-08-22 14:04:25 +0200

    Main_menu-spinner_v1

--- frontend/src/app/components/main-menu/functions/notes/show-note/show-note.component.ts
+++ frontend/src/app/components/main-menu/functions/notes/show-note/show-note.component.ts
@@ -3,6 +3,7 @@ import { Note } from 'src/app/models/note.model';
 import { NotesService } from 'src/app/services/notes.service';
 import { ActivatedRoute } from '@angular/router';
 import {Subscription} from 'rxjs';
+import { tap,delay} from 'rxjs/operators';
 
 @Component({
   selector: 'app-show-note',
@@ -14,6 +15,7 @@ export class ShowNoteComponent implements OnInit,OnDestroy{
 
 constructor(private noteService:NotesService, private activatedRoute:ActivatedRoute) {}
 
+isLoading:Boolean = true;
 
 ngOnInit(): void {
   let id:string = "";
@@ -31,13 +33,21 @@ note!:Note;
 
   getNote(id:string)
   {
-    this.noteService.getNoteById(id).subscribe((res)=>{
+
+
+  this.noteService.getNoteById(id).pipe(
+  delay(2000))
+  .subscribe((res)=>{
+
+      this.isLoading = false;
       this.note = res;
       console.log(this.note);
     },
     (error)=>{
+      this.isLoading = false;
       console.log(error)
     })
+
   }
 
 }
