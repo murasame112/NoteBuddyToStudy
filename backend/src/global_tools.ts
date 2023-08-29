c70ef077f4af6179078e8e48e024006f4617ba4b
murasame112 <tomaszwiesek00@gmail.com>
2023-08-29 13:06:24 +0200

    funkcje dat

--- backend/src/global_tools.ts
+++ backend/src/global_tools.ts
@@ -1,18 +1,29 @@
 import { ObjectId } from "bson";
 
-export function createDateString(date?: Date){
-	if(typeof date !== 'undefined'){
-		return new Date(date).toLocaleString("en-GB");
-	}else{
-		return new Date().toLocaleString("en-GB");
-	}
+export function createDateString(date?: Date) {
+  if (typeof date !== "undefined") {
+    return new Date(date).toLocaleString("en-GB");
+  } else {
+    return new Date().toLocaleString("en-GB");
+  }
 }
 
-export function createDate(str?: Date){
-	if(typeof str !== 'undefined'){
-		// przerobić str
-		//return new Date(str);
-	}else{
-		//return new Date();
-	}
+export function createDate(str?: string) {
+  if (typeof str !== "undefined") {
+    let strArr = str.split(",");
+    let dateArr = strArr[0].split("/");
+    let correctStr = "";
+    for (let i = 2; i >= 0; i--) {
+      correctStr += dateArr[i];
+      if (i != 0) {
+        correctStr += "-";
+      }
+    }
+    correctStr += strArr[1];
+
+    return new Date(correctStr);
+  } else {
+    // probably won't be used, but just in case
+    return new Date();
+  }
 }
