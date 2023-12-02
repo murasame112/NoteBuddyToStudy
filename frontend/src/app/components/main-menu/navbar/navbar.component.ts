bee5be3e4a60de620c479b62fcea0287c7f0f55c
mstasiowski <michal.stasiowski@microsoft.wsei.edu.pl>
2023-12-03 00:22:13 +0100

    twms-zmiana_dzialania_navbara

--- frontend/src/app/components/main-menu/navbar/navbar.component.ts
+++ frontend/src/app/components/main-menu/navbar/navbar.component.ts
@@ -3,118 +3,83 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit{
-
-  navbarState:boolean = false;
-
+export class NavbarComponent implements OnInit {
   ngOnInit(): void {
-    const navContainer:any = document.querySelector('#containerNav');
-    let navBtn:any = document.querySelectorAll('.nav-btn');
-    let profileBtn:any = document.querySelector('#profile-btn');
-    let settingsBtn:any = document.querySelector('#settings-btn');
-    let hamburgerBar:any =document.querySelector('#hamburgerBar');
-    const observer:any = new ResizeObserver((entires)=>{
-      if(entires[0].borderBoxSize[0].inlineSize>650)
-      {
-        navBtn.forEach((e:any)=> {
-          e.style.display="block";
+    this.Observers();
+  }
+
+  Observers() {
+    const navContainer: any = document.querySelector('#containerNav');
+    let hamburger_nav_btns: any = document.querySelectorAll(
+      '.hamburger-nav-btns'
+    );
+    const observer: any = new ResizeObserver((entires) => {
+      if (entires[0].borderBoxSize[0].inlineSize > 650) {
+        hamburger_nav_btns.forEach((e: any) => {
+          e.style.display = 'none';
         });
-
-
-        profileBtn.style.top = `${6}rem`;
-        settingsBtn.style.top = `${8}rem`;
-        hamburgerBar.style.height =`${12}rem`;
-
-
-        console.log('done');
-      }else
-      {
-        if(this.navbarState ==false)
-        {
-          navBtn.forEach((e:any)=> {
-            e.style.display="none";
-          });
-
-        }
-
+        // console.log('window > 650 ');
+      } else {
+        // console.log('window < 650');
+        hamburger_nav_btns.forEach((e: any) => {
+          e.style.display = 'block';
+        });
       }
     });
-
-    const observer2:any = new ResizeObserver((entires)=>{
-      if(entires[0].borderBoxSize[0].inlineSize<650)
-      {
-
-        profileBtn.style.top = `${15}rem`;
-        settingsBtn.style.top = `${17}rem`;
-        hamburgerBar.style.height =`${25}rem`;
-
-      }
-    })
 
     observer.observe(navContainer);
-    observer2.observe(navContainer);
   }
 
+  menuBarOpen() {
+    let hamburgerBar: any = document.querySelector('#hamburgerBar');
+    let hamb_menu: any = document.querySelector('#hamb-menu');
+    let hamburgerIcon: any = document.querySelector('#hamburgerIcon');
+    let hamburgerIconClose: any = document.querySelector(
+      '#hamburgerIcon-close'
+    );
+    let menuBtn: any = document.querySelectorAll('.menu-btn');
+    let hamburger_nav_btns: any = document.querySelectorAll(
+      '.hamburger-nav-btns'
+    );
 
+    hamburgerBar.style.display = 'block';
+    hamb_menu.style.display = 'flex';
+    hamburgerIcon.style.visibility = 'hidden';
+    hamburgerIconClose.style.display = 'block';
 
-  menuBarOpen()
-  {
-    this.navbarState = true;
-    let hamburgerBar:any = document.querySelector('#hamburgerBar');
-    let hamburgerIcon:any = document.querySelector('#hamburgerIcon');
-    let hamburgerIconClose:any = document.querySelector('#hamburgerIcon-close');
-    let navBtn:any = document.querySelectorAll('.nav-btn');
-    let menuBtn:any = document.querySelectorAll('.menu-btn');
-
-    hamburgerBar.style.display = "block";
-    hamburgerIcon.style.display = "none";
-    hamburgerIconClose.style.display = "block";
-
-
-    menuBtn.forEach((e:any)=> {
-      e.style.display="block";
+    menuBtn.forEach((e: HTMLElement) => {
+      e.style.display = 'block';
     });
 
-
-    if(window.innerWidth<=650)
-    {
-
-    navBtn.forEach((e:any)=> {
-      e.style.display="block";
-    });
-
+    if (window.innerWidth > 650) {
+      hamburger_nav_btns.forEach((e: any) => {
+        e.style.display = 'none';
+      });
+    } else {
+      hamburger_nav_btns.forEach((e: any) => {
+        e.style.display = 'block';
+      });
+    }
   }
 
+  menuBarClose() {
+    let hamburgerBar: any = document.querySelector('#hamburgerBar');
+    let hamb_menu: any = document.querySelector('#hamb-menu');
+    let hamburgerIcon: any = document.querySelector('#hamburgerIcon');
+    let hamburgerIconClose: any = document.querySelector(
+      '#hamburgerIcon-close'
+    );
+    let menuBtn: any = document.querySelectorAll('.menu-btn');
 
-  }
+    hamburgerBar.style.display = 'none';
+    hamb_menu.style.display = 'none';
+    hamburgerIcon.style.visibility = 'visible';
+    hamburgerIconClose.style.display = 'none';
 
-  menuBarClose()
-  {
-    this.navbarState= false;
-    let hamburgerBar:any = document.querySelector('#hamburgerBar');
-    let hamburgerIcon:any = document.querySelector('#hamburgerIcon');
-    let hamburgerIconClose:any = document.querySelector('#hamburgerIcon-close');
-    let navBtn:any = document.querySelectorAll('.nav-btn');
-    let menuBtn:any = document.querySelectorAll('.menu-btn');
-
-    hamburgerBar.style.display = "none";
-    hamburgerIcon.style.display = "block";
-    hamburgerIconClose.style.display = "none";
-
-    menuBtn.forEach((e:any)=> {
-      e.style.display="none";
+    menuBtn.forEach((e: any) => {
+      e.style.display = 'none';
     });
-
-    if(window.innerWidth<=650)
-    {
-
-    navBtn.forEach((e:any)=> {
-      e.style.display="none";
-    });
-
   }
-}
-
 }
