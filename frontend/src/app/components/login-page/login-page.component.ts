9d56929b7f264359df53218b7b0d0655b8531077
mstasiowski <michal.stasiowski@microsoft.wsei.edu.pl>
2023-12-15 17:50:08 +0100

    twms-wyglad_fiszek_validacja_loginu

--- frontend/src/app/components/login-page/login-page.component.ts
+++ frontend/src/app/components/login-page/login-page.component.ts
@@ -1,23 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { takeUntil } from 'rxjs';
 import { CustomValidators } from 'src/app/helpers/custom-validators';
+import { Unsubscribe } from 'src/app/helpers/unsubscribe.class';
+import { Login } from 'src/app/models/login.model';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent extends Unsubscribe implements OnInit {
+  loginForm!: FormGroup;
 
-  loginForm!:FormGroup
+  constructor(private usersService: UsersService) {
+    super();
+  }
 
   ngOnInit(): void {
-    this.loginForm = new FormGroup(
-     {
-       login: new FormControl('',[Validators.required,CustomValidators.spaceValidator(),CustomValidators.specialCharactersValidator()]),
-       password: new FormControl('',[Validators.required,CustomValidators.spaceValidator(),CustomValidators.passwordValidation()]),
+    this.loginForm = new FormGroup({
+      login: new FormControl('', [
+        Validators.required,
+        CustomValidators.spaceValidator(),
+        CustomValidators.specialCharactersValidator(),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        CustomValidators.spaceValidator(),
+        CustomValidators.passwordValidation(),
+      ]),
+    });
+  }
 
-     }
-   )
- }
+  login(data: Login) {
+    console.log(`login: ${data.login} hasło: ${data.password}`);
+
+    let login: any = {
+      email: data.login,
+      password: data.password,
+    };
+
+    if (this.loginForm.valid) {
+      console.log('logujemy');
+    } else {
+      console.log('nie logujemy');
+    }
+
+    // console.log(login);
+    // this.usersService
+    //   .loginUser(login)
+    //   .pipe(takeUntil(this.unsubscribe$))
+    //   .subscribe((result: any) => {
+    //     console.log('login result:', result);
+    //   });
+  }
 }
