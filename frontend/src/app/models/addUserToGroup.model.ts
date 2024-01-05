6bd220f8dfe530bef805895632100400728b3715
mstasiowski <michal.stasiowski@microsoft.wsei.edu.pl>
2024-01-05 19:22:25 +0100

    twms-groups_system

---
+++ frontend/src/app/models/addUserToGroup.model.ts
@@ -0,0 +1,5 @@
+export interface AddUserToGroup {
+  type: 'two' | 'multiple';
+  user_id: string;
+  subcategory_id: string;
+}
---
+++ frontend/src/app/services/groups.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Group } from '../models/group.model';
+import { AddUserToGroup } from '../models/addUserToGroup.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GroupsService {
+  constructor(private http: HttpClient) {}
+
+  public apiUrl = 'http://localhost:3000';
+
+  addUserToGroup(group: AddUserToGroup): Observable<AddUserToGroup> {
+    const url = `${this.apiUrl}/addtogroup`;
+    return this.http.patch<AddUserToGroup>(url, group);
+  }
+
+  // getHint(): Observable<Group[]> {
+  //   const url = `${this.apiUrl}/hints`;
+  //   return this.http.get<Group[]>(url);
+  // }
+
+  // getHintById(id: string): Observable<Group> {
+  //   const url = `${this.apiUrl}/hint/${id}`;
+  //   return this.http.get<Group>(url);
+  // }
+
+  // updateHint(hint: Group): Observable<Group> {
+  //   const url = `${this.apiUrl}/hint/${hint._id}`;
+  //   return this.http.put<Group>(url, hint);
+  // }
+
+  // deleteHint(id: string): Observable<{}> {
+  //   const url = `${this.apiUrl}/hint/${id}`;
+  //   return this.http.delete(url);
+  // }
+}
