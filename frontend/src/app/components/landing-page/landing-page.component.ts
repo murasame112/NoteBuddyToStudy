b9cf63b6c3d20f51a5f68c1813e8878a7803f1db
mstasiowski <michal.stasiowski@microsoft.wsei.edu.pl>
2024-01-10 01:41:43 +0100

    twms-landing_changes

--- frontend/src/app/components/landing-page/landing-page.component.ts
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  copyrightYear: number | null = null;
 
+  ngOnInit(): void {
+    this.getYear();
+  }
+
+  getYear() {
+    this.copyrightYear = new Date().getFullYear();
+  }
 }
