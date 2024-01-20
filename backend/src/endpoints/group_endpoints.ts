5176d5d84cfefeb90b79d7c8a7915464a5f5c989
murasame112 <tomaszwiesek00@gmail.com>
2024-01-20 20:18:02 +0100

    res400

--- backend/src/endpoints/group_endpoints.ts
+++ backend/src/endpoints/group_endpoints.ts
@@ -729,7 +729,7 @@ export function addUserToGroup(req: Request, res: Response) {
 			group = new Group(type, [user_id], subcategory_id);
 			addResult = global.insertItem(group, table_name);
 		}else if(value[0].users.some( (element) => element.toString() === user_id.toString() )){
-			res.status(201).send("Error");
+			res.status(400).send("Error");
 			return false;
 		}
 		else{
